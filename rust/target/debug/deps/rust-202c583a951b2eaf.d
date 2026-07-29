fd8cfd1d25ed8320845042e7c88e204dbaa9319d
BarbaraCss <barbara.casas@bellevuecollege.edu>
2026-07-28 22:20:07 -0700

    Add language templates and VS Code settings

---
+++ rust/target/debug/deps/rust-202c583a951b2eaf.d
@@ -0,0 +1,5 @@
+/home/BarbaraCss/coding-workspace/rust/target/debug/deps/rust-202c583a951b2eaf.d: src/main.rs
+
+/home/BarbaraCss/coding-workspace/rust/target/debug/deps/librust-202c583a951b2eaf.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/deps/rust-5d06ed18468dda4f.d
@@ -0,0 +1,5 @@
+/home/BarbaraCss/coding-workspace/rust/target/debug/deps/rust-5d06ed18468dda4f.d: src/main.rs
+
+/home/BarbaraCss/coding-workspace/rust/target/debug/deps/librust-5d06ed18468dda4f.rmeta: src/main.rs
+
+src/main.rs:
